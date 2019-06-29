0c295513fe53ce2ed64f3207ad3b2a6e4fbc119a
Zak Clark <zak@zakariahclark.com>
2019-06-28 18:46:09 -0700

    Small changes

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,12 @@
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://zak:Iampowerful1211%21@cluster0-wmxwc.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://zak:Iampowerful1211%21@cluster0-wmxwc.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 
-// module.exports="mongodb+srv://zak:Iampowerful1211%21@cluster0-wmxwc.mongodb.net/test?retryWrites=true&w=majority"
+module.exports="mongodb+srv://zak:Iampowerful1211%21@cluster0-wmxwc.mongodb.net/test?retryWrites=true&w=majority"
